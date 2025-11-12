e73c374be90ad7d57caee563ca856441dfa163e1
John-Willy-Brandt <67523398+John-Willy-Brandt@users.noreply.github.com>
2025-11-12 22:13:10 +0900

    25.11.12 Last

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,18 @@
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+   validates :nickname, presence: true
+   validates :family_name, presence: true,
+                          format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/,
+                                    message: 'は全角の漢字・ひらがな・カタカナで入力してください（空白不可）' }
+   validates :first_name, presence: true,
+                          format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/,
+                                    message: 'は全角の漢字・ひらがな・カタカナで入力してください（空白不可）' }
+  validates :family_name_kana, presence: true,
+                             format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナで入力してください' }
+   validates :first_name_kana, presence: true,
+                            format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナで入力してください' }
+   validates :birthday, presence: true
+
 end
