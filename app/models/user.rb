a1085dd77457df51f14380a926d80e22bd615e18
John-Willy-Brandt <67523398+John-Willy-Brandt@users.noreply.github.com>
2025-11-15 13:49:48 +0900

    25.11.15 for review

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
- class User < ApplicationRecord
+class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -8,17 +8,18 @@
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-   validates :nickname, presence: true
-   validates :family_name, presence: true,
+  validates :nickname, presence: true
+  validates :family_name, presence: true,
                           format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/,
                                     message: 'は全角の漢字・ひらがな・カタカナで入力してください（空白不可）' }
-   validates :first_name, presence: true,
-                          format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/,
-                                    message: 'は全角の漢字・ひらがな・カタカナで入力してください（空白不可）' }
+  validates :first_name, presence: true,
+                         format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/,
+                                   message: 'は全角の漢字・ひらがな・カタカナで入力してください（空白不可）' }
   validates :family_name_kana, presence: true,
-                             format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナで入力してください' }
-   validates :first_name_kana, presence: true,
-                            format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナで入力してください' }
-   validates :birthday, presence: true
+                               format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナで入力してください' }
+  validates :first_name_kana, presence: true,
+                              format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナで入力してください' }
+  validates :birthday, presence: true
 
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: 'は半角英数字混合で入力してください' }
 end
