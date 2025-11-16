b9f2fa48ec36012e6a05b69f568289a68509811c
John-Willy-Brandt <67523398+John-Willy-Brandt@users.noreply.github.com>
2025-11-16 19:30:39 +0900

    25.11.16 writing RSPEC

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,23 @@
 FactoryBot.define do
   factory :item do
-    
-  end
+     association :user
+
+    item_name        { 'テスト商品' }
+    explanation      { 'テスト商品です。' }
+    category_id      { 2 }  # 1 は '---' にしているので 2 以上
+    status_id        { 2 }
+    delivery_cost_id { 2 }
+    prefecture_id    { 2 }
+    delivery_duration_id { 2 }
+    price            { 300 }
+
+    # 画像のダミー
+    after(:build) do |item|
+      item.image.attach(
+        io: File.open(Rails.root.join('public/images/test_image.png')),
+        filename: 'test_image.png',
+        content_type: 'image/png'
+      )
+    end
+    end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
-
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+ let(:item) { FactoryBot.build(:item) }
+
+  describe '商品出品機能' do
+    context '出品できるとき' do
+      it '全ての項目が正しければ出品できる' do
+        expect(item).to be_valid
+      end
+    end
+
+
+
+
 end
