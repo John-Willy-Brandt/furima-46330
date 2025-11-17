0dafd3c9084652f1f16a8d410215d85994ad841f
John-Willy-Brandt <67523398+John-Willy-Brandt@users.noreply.github.com>
2025-11-17 18:50:53 +0900

    25.11.17 After fixing small bug

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -93,6 +93,13 @@ RSpec.describe Item, type: :model do
         item.valid?
         expect(item.errors.full_messages).to include('Price is not a number')
       end
+
+      it 'user が紐付いていないと保存できないこと' do
+      item.user = nil
+      item.valid?
+      expect(item.errors.full_messages).to include('User must exist')
+      end
+
     end
   end
 
@@ -134,11 +141,6 @@ RSpec.describe Item, type: :model do
       )
     end
 
-    it 'user が紐付いていないと保存できないこと' do
-    item.user = nil
-    item.valid?
-    expect(item.errors.full_messages).to include('User must exist')
-    end
 
   end
 end
