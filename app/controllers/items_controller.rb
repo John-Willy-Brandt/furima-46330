09abde023573d851472d3510c6fa17bceb00eb37
John-Willy-Brandt <67523398+John-Willy-Brandt@users.noreply.github.com>
2025-11-17 21:59:00 +0900

    25.11.17 tasks doin in Ashikaga Orihime DF

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
+    @items = Item.all.order(created_at: :desc)
   end
 
   def new
@@ -17,6 +18,11 @@ class ItemsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
 
   private
 
