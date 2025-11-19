cb05c0b9816563e3a4d2283d676299fd8fb02267
John-Willy-Brandt <67523398+John-Willy-Brandt@users.noreply.github.com>
2025-11-19 21:36:22 +0900

    Revert "25.11.19 Mainly fix the edit.html.erb"
    This reverts commit 03a0f26c1b572e97f945b47a16f6c24e813b6b09.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,57 +1,40 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create]
 
-  # 一覧表示
   def index
     @items = Item.all.order(created_at: :desc)
   end
 
-  # 詳細表示
-  def show
-    # @item は set_item で取得済み
-  end
-
-  # 新規出品フォーム
   def new
     @item = Item.new
   end
 
-  # 新規登録（DBに保存）
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to root_path   # トップ（index）に戻る
+      redirect_to root_path, notice: '商品を出品しました。'
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  # 編集フォーム
-  def edit
-    # @item は set_item で取得済み
-  end
-
-  # 更新処理
-  def update
-    if @item.update(item_params)
-      redirect_to item_path(@item)
-    else
-      render :edit, status: :unprocessable_entity
-    end
+  def show
+    @item = Item.find(params[:id])
   end
 
   private
 
-  def set_item
-    @item = Item.find(params[:id])
-  end
-
   def item_params
     params.require(:item).permit(
-      :item_name, :explanation, :category_id, :status_id,
-      :delivery_cost_id, :prefecture_id, :delivery_duration_id,
-      :price, :image
+      :image,
+      :item_name,
+      :explanation,
+      :category_id,
+      :status_id,
+      :delivery_cost_id,
+      :prefecture_id,
+      :delivery_duration_id,
+      :price
     ).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-
-
   devise_for :users
   get 'items/index'
   root 'items#index' # This makes it the homepage
 
   get 'items/new', to: 'items#new'
 
-  resources :items
+  resources :items, only: [:index, :new, :create, :show]
 end
