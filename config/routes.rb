03a0f26c1b572e97f945b47a16f6c24e813b6b09
John-Willy-Brandt <67523398+John-Willy-Brandt@users.noreply.github.com>
2025-11-19 18:42:57 +0900

    25.11.19 Mainly fix the edit.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+
+
   devise_for :users
   get 'items/index'
   root 'items#index' # This makes it the homepage
 
   get 'items/new', to: 'items#new'
 
-  resources :items, only: [:index, :new, :create, :show]
+  resources :items
 end
